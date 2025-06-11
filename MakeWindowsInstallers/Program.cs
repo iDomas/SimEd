54d46d1a42e2eeeaa6681ec70a97ef7b92504ffe
Ciprian Khlud <ciprian.mustiata@gmail.com>
2025-06-11 19:06:13 +0300

    Make installers with one tool.

---
+++ MakeWindowsInstallers/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var bundler = new ProjectBundler();
+
+Directory.SetCurrentDirectory(@"C:\oss\SimEd\");
+
+string[] platforms = ["win-arm64", "win-x64", "win-x86"];
+foreach (string platform in platforms)
+{
+    await bundler.Bundle("SimEd", platform);
+}
+
+
+Console.WriteLine("Hello, World!");
---
+++ MakeWindowsInstallers/ProjectBundler.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+
+class ProjectBundler
+{
+    public async Task Bundle(string project, string platformName)
+    {
+        string arguments = $"publish {project} -r {platformName} -c Release";
+        var p = Process.Start(new ProcessStartInfo()
+        {
+            FileName = "dotnet",
+            Arguments = arguments,
+            RedirectStandardOutput = true
+        });
+        await p.WaitForExitAsync();
+        var output = await p.StandardOutput.ReadToEndAsync();
+        var lines = output.Split(Environment.NewLine);
+        var publishedLine = lines.LastOrDefault(line => line.Contains("publish"));
+        var path = publishedLine.Split(" -> ")[1].Trim();
+
+        var allowedExtensions = new[] { ".dll", ".exe" };
+        var filesToZip = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories)
+            .Where(f => allowedExtensions.Contains(Path.GetExtension(f)));
+        
+        var outputZipFile = $"{project}_{platformName}.zip";
+        File.Delete(outputZipFile);
+        ZipFileCreator.CreateZipFile(outputZipFile, filesToZip);
+    }
+}
